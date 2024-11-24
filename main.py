7dffc7ce80db9f21a1b73c9a9004dfa22f643cd7
SamTenpenny <samuel.tenpenny@outlook.com>
2024-11-24 01:27:16 -0500

    Added asteroid field and objects. Added spawning and movement to asteroids

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import pygame
+from asteroidfield import *
 from constants import *
 from player import Player
 
-
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+asteroids = pygame.sprite.Group()
+Player.containers = (asteroids, updatable, drawable)
+Asteroid.containers = (asteroids, updatable, drawable)
+AsteroidField.containers = (updatable,)
 
 def main():
     pygame.init()
@@ -10,16 +16,21 @@ def main():
     clock = pygame.time.Clock()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
+        for item in updatable:
+            item.update(dt)
         
         screen.fill("black")
-        player.draw(screen)
+
+        for item in drawable:
+            item.draw(screen)
+        
         pygame.display.flip()
         
         # limit framerate to 60 FPS
